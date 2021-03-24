a6cd311118f96abf84f24923ba9d4a6735234a33
SOVEREIGN <isimemeni@gmail.com>
2021-03-24 12:26:21 -0400

    working on main page of Pokemon app

--- pokedex/src/Pages/MainPage.js
+++ pokedex/src/Pages/MainPage.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
-const MainPage = () => {
+class MainPage extends React.Component {
+
+    state = {
+        pokemon: []
+    }
+
+componentDidMount () {
+    fetch("https://pokeapi.co/api/v2/pokemon/")
+    .then(res => res.json())
+    .then(response => this.setState({
+        pokemon: response
+    }))
+
+    .catch(err => console.log(err));
+}
+
+render() {
+    const [pokemon] = this.state
+    console.log(pokemon)
     return (
-        <div>
-            
-        </div>
-    );
-};
+        <div></div>
+    )
+}
+
+}
 
 export default MainPage;
