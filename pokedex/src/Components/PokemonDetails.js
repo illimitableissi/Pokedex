e65ac20dfdec872b14ad09e58d627d607964577c
SOVEREIGN <isimemeni@gmail.com>
2021-03-24 16:13:50 -0400

    fixed app api calls

---
+++ pokedex/src/Components/PokemonDetails.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const PokemonDetails = (props) => {
+    return (
+        <div>
+            <Card style={{width: '11rem' }}>
+                <Card.Img variant="top" src={props.image} />
+                <Card.Body>
+                    <Card.Title>{props.name}</Card.Title>
+                    <Card.Text>
+                        ID: {props.id}
+                    </Card.Text>
+                    <Card.Text>
+                        Experience: {props.information}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+};
+
+export default PokemonDetails;
--- pokedex/src/Pages/MainPage.js
+++ pokedex/src/Pages/MainPage.js
@@ -1,26 +1,56 @@
 import React from 'react';
+import PokemonDetails from '../Components/PokemonDetails'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
 
 class MainPage extends React.Component {
 
     state = {
-        pokemon: []
+        pokemon: [],
+        pokemonEntries: [],
     }
 
 componentDidMount () {
-    fetch("https://pokeapi.co/api/v2/pokemon/")
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=151/")
     .then(res => res.json())
     .then(response => this.setState({
-        pokemon: response
-    }))
-
+        pokemon: response.results}, () => {
+        this.state.pokemon.map(i => {
+            fetch(i.url)
+            .then(res => res.json())
+            .then(data => {
+                var temp = this.state.pokemonEntries
+                temp.push(data)
+                this.setState({pokemonEntries: temp})
+              }
+            )            
+        .catch(err => console.log(err));
+        })
+    }
+    ))
+    
     .catch(err => console.log(err));
 }
 
 render() {
-    const [pokemon] = this.state
-    console.log(pokemon)
+    const {pokemon, pokemonEntries, pokemonUrls} = this.state
     return (
-        <div></div>
+        <div>
+            <h1>Pokedex</h1>
+            <Container>
+                <Row>
+            {pokemonEntries.map( j =>
+            <PokemonDetails 
+                image = {j.sprites['front_default']}
+                name = {j.name.toUpperCase()}
+                id = {j.id}
+                information = {j.base_experience}
+            />
+                )}
+            </Row>
+            </Container>
+        </div>
     )
 }
 
